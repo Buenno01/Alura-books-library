88da16beed495ea42705ef373969a0d036bb0ebe
Vinícius Bueno Costa <120047535+Buenno01@users.noreply.github.com>
2023-04-07 14:09:08 -0300

    added reduce method and corrected the available filter

--- app/filter.js
+++ app/filter.js
@@ -1,13 +1,25 @@
-const botoes = document.querySelectorAll('.btn')
+const botoes = document.querySelectorAll('.btn');
+const elementoComValorTotal = document.getElementById('valor_total_livros_disponiveis');
+
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
     if (this.classList.value == 'btn btn-disponiveis'){
         let disponiveis = livros.filter(livro => livro.quantidade > 0)
-        exibirOsLivrosNaTela(disponiveis)
+        exibirOsLivrosNaTela(disponiveis);
+        const valorTotal = calcularValorTotal(livrosFiltrados)
+        exibirValorTotal(valorTotal);
     } else {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
     let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
 }}
+
+function exibirValorTotal(valorTotal) {
+    elementoComValorTotal.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+}
